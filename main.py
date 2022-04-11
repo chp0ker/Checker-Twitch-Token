7cafa597cb29d4e93476c00bbfbaccb4d20f668f
chp0ker <96289868+chp0ker@users.noreply.github.com>
2022-04-11 17:39:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,51 @@
-import aiohttp
-import asyncio
-import datetime
-import os
+import queue
+import threading
 import time
 
+import requests
+num_worker_threads = 100
+start_time = time.time()
 
-async def main(date_dir, tokens):
-    async with aiohttp.ClientSession() as session:
-        for i in range(0, len(tokens)):
-            headers = {"Authorization": f"OAuth {tokens[i]}"}
-            response = await session.get("https://id.twitch.tv/oauth2/validate", headers=headers)
-            if response.status == 200:
-                open(f"{date_dir}/good-tokens.txt", "a").write(f"{tokens[i]}\n")
-            else:
-                open(f"{date_dir}/bad-tokens.txt", "a").write(f"{tokens[i]}\n")
-    open(f"{date_dir}/all-tokens.txt", "w").write(open("all-tokens.txt", "r").read())
+tokens = open("all-tokens.txt", "r", encoding="utf-8").read().splitlines()
+good = open(f"good-tokens.txt", "a")
+bad = open(f"bad-tokens.txt", "a")
+
+def main(token):
+    headers = {"Authorization": f"OAuth {token}"}
+    response = requests.get("https://id.twitch.tv/oauth2/validate", headers=headers)
+    if response.status_code == 200:
+        good.write(f"{token}\n")
+    else:
+        bad.write(f"{token}\n")
 
 
-if __name__ == '__main__':
-    start_time = time.time()
-    date = datetime.datetime.now().strftime("%m.%d.%Y-%H.%M.%S")
-    os.mkdir(date)
-    all_tokens = open("all-tokens.txt", "r").read().splitlines()
-    asyncio.get_event_loop().run_until_complete(main(date, all_tokens))
-    print("--- %s seconds ---" % (time.time() - start_time))
+def worker():
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        main(item)
+        q.task_done()
+
+
+q = queue.Queue()
+
+threads = []
+
+for i in range(num_worker_threads):
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
+
+for item in tokens:
+    q.put(item)
+
+q.join()
+
+for i in range(num_worker_threads):
+    q.put(None)
+
+for t in threads:
+    t.join()
+
+print("--- %s seconds ---" % (time.time() - start_time))
